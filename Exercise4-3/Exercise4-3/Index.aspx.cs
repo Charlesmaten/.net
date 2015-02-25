7d3c8349e5d181cdd1200840513d74f4aec1288c
Charlesmaten <Eagle_day@hotmail.com>
2015-02-25 08:55:41 +0100

    exercise 4-3 added

---
+++ Exercise4-3/Exercise4-3/Index.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise4_3
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //hvis det er første gange
+            if (!Page.IsPostBack)
+            {
+                ListItem a = new ListItem("Abby", "135");
+                ListItem b = new ListItem("Bob", "175");
+                ListItem c = new ListItem("Charlie", "55");
+                ListItem d = new ListItem("Dawn", "45");
+
+            }
+        }
+    }
+}

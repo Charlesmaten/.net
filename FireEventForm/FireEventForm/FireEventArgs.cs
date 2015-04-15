95d9527b13721667edf76b3bb3c6eae2675a6288
Charlesmaten <Eagle_day@hotmail.com>
2015-04-15 08:48:08 +0200

    fireeventform tilføjet?

---
+++ FireEventForm/FireEventForm/FireEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireEventForm
+{
+
+    //must be public : In windows Form Application classes are created without public
+    //=Internal = only accesible from other classes in same assembly
+    public class FireEventArgs : EventArgs
+    {
+        //Inherits EventArgs to be able to use future extentions to Eventargs
+
+        //event information
+        public int location;
+        public string room;
+        public int ferocity;
+
+        //Constructor
+        public FireEventArgs(int location, string room, int ferocity)
+        {
+            this.location = location;
+            this.room = room;
+            this.ferocity = ferocity;
+        }
+
+    }
+}
---
+++ FireEventForm/FireEventForm/FireStation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FireEventForm
+{
+    public partial class FireStation : Form
+    {
+        public delegate void FireEventHandler(object sender, FireEventArgs fire)
+
+        public event FireErrorEventHandler fireEvent;
+
+        public FireStation(){
+            InitializeComponent();
+            StartPosition = FormStartPosition.Manual;
+            Location = new Point(200, 200);
+            textBox1.BackColor = Color.Yellow;
+            textBox1.Text = "";
+            button1.Enabled = false;
+
+            listBox1.Items.Add("Fire station ready");
+        }
+
+        //Raise event
+        public void RaiseFireAlarm(int location, string room, int ferocity)
+        {
+            listBox1.Items.Add("Alarm from ") + location + " : " + room + ", ferocity" + ferocity;
+            if(ferocity == 3)
+            {
+                textBox1.BackColor = Color.Red;
+                textBox1.Text = "Alarm";
+                button1.Enabled = true;
+            }
+            FireEventArgs fea = new FireEventArgs(location, room, ferocity);
+            fireEvent(this, fea);
+        }
+
+
+        public FireStation()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

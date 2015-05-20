4f692d7bdb4f4339bd6a78656aeff4021982cf5e
Charlesmaten <Eagle_day@hotmail.com>
2015-05-20 10:58:25 +0200

    MvcSnake tilføjet

---
+++ MVCSnakeDB/MVCSnakeDB/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCSnakeDB.Models;
+
+namespace MVCSnakeDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(SelectSnakeModel mySelectSnakeModel)
+        {
+            DataSet ds = mySelectSnakeModel.GetAllSnakes();
+            ViewBag.Snakelist = ds.Tables[0];
+            return View();
+        }
+
+    }
+}
---
+++ MVCSnakeDB/MVCSnakeDB/Models/SelectSnakeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVCSnakeDB.Models
+{
+    //Only one method in class
+    public class SelectSnakeModel
+    {
+        public DataSet GetAllSnakes()
+        {
+            SqlConnection conn = new SqlConnection(@"data source = .\sqlexpress; integrated security = true; database = SnakeDB");
+            SqlCommand cmd = new SqlCommand("SELECT SnakeID, Type, Name, Length, Owner from Snake", conn);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
